e0a5b24c296588744d41c15170102ecd5977f961
Ibramadi75 <98467166+Ibramadi75@users.noreply.github.com>
2023-03-23 11:21:27 +0100

    Fix salary treatement in employee schema

--- server/Employee.js
+++ server/Employee.js
@@ -7,7 +7,13 @@ const employeeSchema = new Schema({
     address : String,
     service : String,
     socialSecurityNumber : Number,
-    grossSalary : mongoose.Decimal128,
+    salary : {
+        amount : mongoose.Decimal128,
+        type : {
+            type : 'String',
+            enum : ['Salaire de base', 'Avantages en nature', 'Primes', 'Gratifications', 'Indemnités de transport', 'Indemnités de repas', 'Indemnités de logement', 'Indemnités de départ à la retraite', 'Indemnités de licenciement', 'Indemnités de congés payées', 'Remboursements de frais professionnels']
+        }
+    },
     hiringDate : { type : Date, default : Date.now() },
     endDateContract : { type : Date, default : null }
 },{
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import { Employee } from "./Employee.js";
+import { Wage } from "./Wage.js";
 import mongoose from "mongoose";
 
 main().catch(err => console.log(err));
@@ -6,6 +7,17 @@ main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/SalaryManagement-DB');
 
+
+    // db.Employees.insertOne({
+    //     name : "Ibrahim",
+    //     surname : "Madi",
+    //     adress : "37 avenue de Mongo, Paris",
+    //     service : "IT",
+    //     socialSecurityNumber : "321",
+    //     grossSalary : "1188.37",
+    //     HiringDate : Date.now(),
+    //     endDateContract : null,
+    // });
 
     // db.Employees.insertOne({
     //     name : "Ibrahim",
