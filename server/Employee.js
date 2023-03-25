06cc3fca43ca6784cd177e5645bbadfda91cd66d
Ibramadi75 <98467166+Ibramadi75@users.noreply.github.com>
2023-03-25 12:45:13 +0100

    ins and totalSalary()

--- server/Employee.js
+++ server/Employee.js
@@ -11,7 +11,7 @@ const employeeSchema = new Schema({
         amount : mongoose.Decimal128,
         type : {
             type : 'String',
-            enum : ['Salaire de base', 'Avantages en nature', 'Primes', 'Gratifications', 'Indemnités de transport', 'Indemnités de repas', 'Indemnités de logement', 'Indemnités de départ à la retraite', 'Indemnités de licenciement', 'Indemnités de congés payées', 'Remboursements de frais professionnels']
+            enum : ['Salaire de base', 'Avantages en nature', 'Prime', 'Gratifications', 'Indemnités de transport', 'Indemnités de repas', 'Indemnités de logement', 'Indemnités de départ à la retraite', 'Indemnités de licenciement', 'Indemnités de congés payées', 'Remboursements de frais professionnels']
         }
     },
     hiringDate : { type : Date, default : Date.now() },
@@ -20,6 +20,13 @@ const employeeSchema = new Schema({
     methods: {
         findCoworkers() {
           return mongoose.model('Employee').find({ service: this.service });
+        },
+        totalSalary() {
+            let total = 0;
+            for (let i = 0; i < this.grossSalary.length; i++) {
+                total += this.grossSalary[i].amount;
+            }
+            return total;
         }
     }
 });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import { Employee } from "./Employee.js";
-import { Wage } from "./Wage.js";
 import mongoose from "mongoose";
 
 main().catch(err => console.log(err));
@@ -7,29 +6,30 @@ main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/SalaryManagement-DB');
 
+    /* Insert */
+    Employees.insertOne({
+        name : "Ibrahim",
+        surname : "Madi",
+        adress : "37 avenue de Mongo, Paris",
+        service : "IT",
+        socialSecurityNumber : "321",
+        salary : [
+            {
+                amount : 1679.33,
+                type : "Salaire de base"
+            },
+            {
+                amount : 332.67,
+                type : "Prime"
+            }
+        ],
+        HiringDate : Date.now(),
+        endDateContract : null,
+    });
 
-    // db.Employees.insertOne({
-    //     name : "Ibrahim",
-    //     surname : "Madi",
-    //     adress : "37 avenue de Mongo, Paris",
-    //     service : "IT",
-    //     socialSecurityNumber : "321",
-    //     grossSalary : "1188.37",
-    //     HiringDate : Date.now(),
-    //     endDateContract : null,
-    // });
 
-    // db.Employees.insertOne({
-    //     name : "Ibrahim",
-    //     surname : "Madi",
-    //     adress : "37 avenue de Mongo, Paris",
-    //     service : "IT",
-    //     socialSecurityNumber : "321",
-    //     grossSalary : "1188.37",
-    //     HiringDate : Date.now(),
-    //     endDateContract : null,
-    // });
+    /* Find/FindOne */
+    const result = await Employee.findOne({ name : "Ibrahim"}).totalSalary().exec();
 
-    const result = await Employee.findOne({ name : "Ibrahim"}).exec()
-    console.log(result)
+    // console.log(result)
 }
